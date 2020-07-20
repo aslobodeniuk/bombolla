614b8cefac8295ee1a14e8d3dabaee7d99fdd4bd
Aleksandr Slobodeniuk <aslobodeniuk@fluendo.com>
2020-07-20 06:41:09 +0200

    add license: GPLv3

--- bombolla/lba-plugin-system.h
+++ bombolla/lba-plugin-system.h
@@ -1,3 +1,23 @@
+/* la Bombolla GObject shell.
+ * Copyright (C) 2020 Aleksandr Slobodeniuk
+ *
+ *   This file is part of bombolla.
+ *
+ *   Bombolla is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Bombolla is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with bombolla.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #ifndef _BOMBOLLA_PLUGIN_SYSTEM
 #define _BOMBOLLA_PLUGIN_SYSTEM
 #include <glib-object.h>
