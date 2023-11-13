4221cccf36ad79e83ee7bf526e4d5ca9cac5171b
Alexander Slobodeniuk <aslobodeniuk@fluendo.com>
2023-11-13 23:22:31 +0100

    python: use lba-module-scanner

--- bombolla/plugins/python/lba-python.c
+++ bombolla/plugins/python/lba-python.c
@@ -19,13 +19,14 @@
 
 #include "bombolla/lba-plugin-system.h"
 #include "bombolla/lba-log.h"
+#include "bombolla/base/lba-module-scanner.h"
+#include <gmo/gmo.h>
 #include <gmodule.h>
 #include <Python.h>
 #include <dlfcn.h>
 
 typedef struct _LbaPython {
   GObject parent;
-  gchar *plugins_path;
   gboolean initialized;
   void (*py_initialize) ();
   int (*py_run_simple_string) (const char *command);
@@ -37,8 +38,11 @@ typedef struct _LbaPythonClass {
   GObjectClass parent;
 } LbaPythonClass;
 
-static gboolean
-lba_python_eval_file (LbaPython * self, const gchar * filename) {
+GMO_DEFINE_MUTOGENE (lba_python, LbaPython, GMO_ADD_DEP (lba_module_scanner));
+
+static void
+lba_python_eval_file (GObject * gobject, const gchar * filename) {
+  LbaPython *self = gmo_get_LbaPython (gobject);
   FILE *fp;
 
   LBA_LOG ("Evaluating [%s]", filename);
@@ -53,83 +57,9 @@ lba_python_eval_file (LbaPython * self, const gchar * filename) {
      * object. */
     self->py_run_simple_string
         ("print ('loaded class: %s' % GObject.GType(lba_plugin).name)");
-    return TRUE;
   }
 
-  return FALSE;
-}
-
-static GSList *lba_python_modules_scan_path (const gchar * path,
-                                             GSList * modules_files);
-
-/* This function is recursive */
-static GSList *
-lba_python_modules_scan_path (const gchar * path, GSList * modules_files) {
-  static const char *LBA_PY_PLUGIN_PREFIX = "lba-";
-  static const char *LBA_PY_PLUGIN_SUFFIX = ".py";
-
-  if (!g_file_test (path, G_FILE_TEST_EXISTS)) {
-    g_warning ("Path [%s] doesn't exist", path);
-    return modules_files;
-  }
-
-  if (g_file_test (path, G_FILE_TEST_IS_DIR)) {
-    GError *err;
-    const gchar *file;
-    GDir *dir;
-
-    LBA_LOG ("Scan directory [%s]", path);
-    dir = g_dir_open (path, 0, &err);
-    if (!dir) {
-      g_warning ("Couldn't open [%s]: [%s]", path,
-                 err ? err->message : "no message");
-      if (err)
-        g_error_free (err);
-      return modules_files;
-    }
-
-    while ((file = g_dir_read_name (dir))) {
-      gchar *sub_path = g_build_filename (path, file, NULL);
-
-      /* Step into the directories */
-      if (g_file_test (sub_path, G_FILE_TEST_IS_DIR)) {
-        modules_files = lba_python_modules_scan_path (sub_path, modules_files);
-        g_free (sub_path);
-        continue;
-      }
-
-      /* If not a directory */
-      if (g_str_has_prefix (file, LBA_PY_PLUGIN_PREFIX) &&
-          g_str_has_suffix (file, LBA_PY_PLUGIN_SUFFIX)) {
-        modules_files = g_slist_append (modules_files, sub_path);
-        sub_path = NULL;
-      } else {
-        g_free (sub_path);
-      }
-    }
-
-    g_dir_close (dir);
-  } else {
-    LBA_LOG ("Scan single file [%s]", path);
-    modules_files = g_slist_append (modules_files, g_strdup (path));
-  }
-
-  return modules_files;
-}
-
-static void
-lba_python_scan (LbaPython * self) {
-  GSList *modules_files = NULL;
-  GSList *l;
-
-  g_return_if_fail (self->plugins_path != NULL);
-
-  modules_files = lba_python_modules_scan_path (self->plugins_path, NULL);
-
-  for (l = modules_files; l; l = l->next) {
-    lba_python_eval_file (self, (gchar *) l->data);
-  }
-  g_slist_free_full (modules_files, g_free);
+  return;
 }
 
 static gboolean
@@ -162,6 +92,7 @@ lba_python_fill_vtable_local (LbaPython * self) {
     return FALSE;
   }
 
+  self->py_initialize ();
   return TRUE;
 }
 
@@ -227,61 +158,66 @@ lba_python_fill_vtable (LbaPython * self) {
 }
 
 static void
-lba_python_init (LbaPython * self) {
-  static volatile gboolean once;
+lba_python_constructed (GObject * gobject) {
+  LbaPython *self = gmo_get_LbaPython (gobject);
 
-  if (!self->plugins_path) {
-    self->plugins_path = g_strdup (g_getenv ("LBA_PYTHON_PLUGINS_PATH"));
+  self->py_run_simple_string ("print('Hello from Bombolla home python')");
+  self->py_run_simple_string ("from gi.repository import GObject");
+  self->initialized = TRUE;
 
-    if (!self->plugins_path) {
-      LBA_LOG ("No plugin path is set. Will scan current directory.");
-      self->plugins_path = g_get_current_dir ();
-    }
-  }
+  GMO_CHAINUP (gobject, lba_python, GObject)->constructed (gobject);
+}
 
-  if (!once) {
-    once = TRUE;
-
-    if (!lba_python_fill_vtable (self)) {
-      return;
-    }
-
-    self->py_run_simple_string ("print('Hello from Bombolla home python')");
-    self->py_run_simple_string ("from gi.repository import GObject");
-
-    lba_python_scan (self);
-    self->initialized = TRUE;
+static void
+lba_python_init (GObject * object, LbaPython * self) {
+  if (!lba_python_fill_vtable (self)) {
+    g_warn_if_reached ();
+    return;
   }
 }
 
 static void
 lba_python_dispose (GObject * gobject) {
-  LbaPython *self = (LbaPython *) gobject;
+  LbaPython *self = gmo_get_LbaPython (gobject);
 
   if (self->initialized) {
     self->py_run_simple_string ("print('Bombolla home python goes to sleep')");
   }
+
+  GMO_CHAINUP (gobject, lba_python, GObject)->dispose (gobject);
 }
 
 static void
 lba_python_finalize (GObject * gobject) {
-  LbaPython *self = (LbaPython *) gobject;
+  LbaPython *self = gmo_get_LbaPython (gobject);
 
   if (self->initialized) {
     /* FIXME: if we don't remove all the classes defined in python
      * before LbaPython, the proccess crashes. */
     self->py_finalize ();
   }
+
+  GMO_CHAINUP (gobject, lba_python, GObject)->finalize (gobject);
 }
 
 static void
-lba_python_class_init (LbaPythonClass * klass) {
-  GObjectClass *object_class = (GObjectClass *) klass;
+lba_python_class_init (GObjectClass * object_class, LbaPythonClass * klass) {
+  LbaModuleScannerClass *lms_class;
 
+  object_class->constructed = lba_python_constructed;
   object_class->dispose = lba_python_dispose;
   object_class->finalize = lba_python_finalize;
+
+  lms_class =
+      (LbaModuleScannerClass *) gmo_class_get_mutogene (object_class,
+                                                        lba_module_scanner_get_type
+                                                        ());
+
+  lms_class->plugin_path_env = "LBA_PYTHON_PLUGINS_PATH";
+  lms_class->plugin_prefix = "lba-";
+  lms_class->plugin_suffix = ".py";
+  lms_class->have_file = lba_python_eval_file;
 }
 
-G_DEFINE_TYPE (LbaPython, lba_python, G_TYPE_OBJECT);
 /* Export plugin */
 BOMBOLLA_PLUGIN_SYSTEM_PROVIDE_GTYPE (lba_python);
